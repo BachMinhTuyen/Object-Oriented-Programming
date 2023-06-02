70f643789071682ae832d8e688009c17971ff781
Minh Tuyen <bachminhtuyen2003@gmail.com>
2023-06-02 11:59:18 +0700

    Create a new Product_Warehouse_Exercise

---
+++ Chapter_Classes And Objects/Product_Warehouse_Exercise/KhoSanPham.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Product_Warehouse_Exercise
+{
+    public class KhoSanPham
+    {
+        private string tenKho;
+        private string diaChiKho;
+        List<SanPham> lst = new List<SanPham>();
+
+        public string TenKho { get => tenKho; set => tenKho = value; }
+        public string DiaChiKho { get => diaChiKho; set => diaChiKho = value; }
+        public List<SanPham> Lst { get => lst; set => lst = value; }
+
+        public void nhapDanhSach_TuFileXML(string filename)
+        {
+            XmlDocument read = new XmlDocument();
+            read.Load(filename);
+            XmlNode productNode = read.SelectSingleNode("/khosanpham");
+            tenKho = productNode["ten"].InnerText;
+            diaChiKho = productNode["diachi"].InnerText;
+            XmlNodeList nodelist = read.SelectNodes("/khosanpham/SanPhams/SanPham");
+            foreach (XmlNode node in nodelist)
+            {
+                SanPham sp = new SanPham();
+                sp.MaSP = node["masp"].InnerText;
+                sp.TenSP = node["tensp"].InnerText;
+                sp.NamSX = int.Parse(node["namsx"].InnerText);
+                sp.DonGia = float.Parse(node["gia"].InnerText);
+                sp.LoaiSP = char.Parse(node["loai"].InnerText);
+                lst.Add(sp);
+            }
+        }
+        public double tinhTongTienSanPham()
+        {
+            double tong = 0;
+            foreach (SanPham sp in lst)
+            {
+                tong += sp.DonGia;
+            }
+            return tong;
+        }
+        public void xuatDanhSach()
+        {
+            Console.WriteLine("Tên kho: {0}", tenKho);
+            Console.WriteLine("Địa chỉ kho: {0}", diaChiKho);
+            Console.WriteLine("Tổng tiền: {0}", tinhTongTienSanPham());
+            Console.WriteLine("{0,-10}|{1,-30}|{2,10}|{3,10}|{4,10}", "Mã SP", "Tên SP", "Năm SX", "Đơn Giá", "Loại SP");
+            foreach (SanPham sp in lst)
+            {
+                sp.xuatThongTin();
+            }    
+        }
+        //Sử dụng LinQ
+        public void timSanPhamBanNhieuNhat()
+        {
+            var sanPhamBanNhieuNhat = lst.GroupBy(sp => sp.MaSP).OrderByDescending(g => g.Count()).First();
+            Console.WriteLine("\nSản phẩm được bán nhiều nhất ({0} lần):", sanPhamBanNhieuNhat.Count());
+            foreach (SanPham sp in sanPhamBanNhieuNhat)
+            {
+                sp.xuatThongTin();
+                break;
+            }
+            
+        }
+        public void sapXepTangDan_TheoDonGia()
+        {
+            lst = lst.OrderBy(t => t.DonGia).ToList();
+        }
+    }
+}
---
+++ Chapter_Classes And Objects/Product_Warehouse_Exercise/SanPham.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product_Warehouse_Exercise
+{
+    public class SanPham
+    {
+        private string maSP;
+        private string tenSP;
+        private int namSX;
+        private float donGia;
+        private char loaiSP;
+
+        public string MaSP
+        {
+            get { return maSP; }
+            set
+            {
+                if (value.Length == 6 && value.StartsWith("SP"))
+                    maSP = value;
+                else
+                    maSP = "SP0001";
+            }
+        }
+        public string TenSP { get => tenSP; set => tenSP = value; }
+        public int NamSX { get => namSX; set => namSX = value; }
+        public float DonGia { get => donGia; set => donGia = value; }
+        public char LoaiSP { get => loaiSP; set => loaiSP = value; }
+        public SanPham()
+        {
+            MaSP = "SP0003";
+            TenSP = "xe Toyota Camry";
+            NamSX = 2021;
+            DonGia = 30000;
+            LoaiSP = 'A';
+        }
+        public SanPham(SanPham sp)
+        {
+            MaSP = sp.MaSP;
+            TenSP = sp.TenSP;
+            NamSX = sp.NamSX;
+            DonGia = sp.DonGia;
+            LoaiSP = sp.LoaiSP;
+        }
+        public SanPham(string maSP, string tenSP, int namSX, float donGia, char loaiSP)
+        {
+            MaSP = maSP;
+            TenSP = tenSP;
+            NamSX = namSX;
+            DonGia = donGia;
+            LoaiSP = loaiSP;
+        }
+        public double tinhThanhTien(int soLuong)
+        {
+            const double VAT = 0.1;
+            return soLuong * donGia * (1 + VAT);
+        }
+        public void xuatThongTin()
+        {
+            Console.WriteLine("{0,-10}|{1,-30}|{2,10}|{3,10}|{4,10}", maSP, tenSP, namSX, donGia, loaiSP);
+        }
+    }
+}
